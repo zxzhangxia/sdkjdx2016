6a407903503059368016aad41cdcfc5fe70445d4
a <969143556@qq.com>
2019-05-22 17:48:15 +0800

    5.22

---
+++ 5.22zx/zx/EWS/src/main/java/com.isoft.dao/impl/UserDAOImpl.java
@@ -0,0 +1,76 @@
+package com.isoft.dao.impl;
+
+import com.isoft.dao.IUserDAO;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository("userDAO")
+public class UserDAOImpl implements IUserDAO{
+    @Autowired
+    SqlSessionFactory sessionFactoryBean;
+    @Override
+    public String login(String uname, String upwd) {
+        SqlSession sqlSession = sessionFactoryBean.openSession(true);
+        String statment = "com.isoft.mapping.userMapper.login";
+        Map map=new HashMap();
+        map.put("uname",uname);
+        map.put("upwd",upwd);
+        List<Map<String,Object>> list = sqlSession.selectList(statment,map);
+        if(list.size()>0)
+            return "success";
+        else
+            return "fault";
+
+    }
+
+    @Override
+    public boolean register(String uname, String upwd, String email) {
+        SqlSession sqlSession = sessionFactoryBean.openSession(true);
+        String statment = "com.isoft.mapping.userMapper.register";
+        Map map=new HashMap();
+        map.put("uname",uname);
+        map.put("upwd",upwd);
+        map.put("email",email);
+        int i = sqlSession.insert(statment,map);
+        if(i>0)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public List<Map<String, Object>> findAllUser(int page, int pageSize) {
+        SqlSession sqlSession = sessionFactoryBean.openSession(true);
+        String statment="com.isoft.mapping.userMapper.findAllUser";
+        Map map=new HashMap();
+        map.put("page",(page-1)*pageSize);
+        map.put("pageSize",pageSize);
+        List<Map<String,Object>> list=sqlSession.selectList(statment,map);
+
+        return list;
+    }
+
+    @Override
+    public Map<String, Object> findUserCount() {
+        try {
+            SqlSession sqlSession = sessionFactoryBean.openSession(true);
+            String statment = "com.isoft.mapping.userMapper.findUserCount";
+            Map map = sqlSession.selectOne(statment);
+            return map;
+        }
+        catch (Exception e)
+        {
+
+            e.printStackTrace();;
+        }
+        return  null;
+    }
+
+}
---
+++ 5.22zx/zx/EWS/src/main/java/com/isoft/service/IUserService.java
@@ -0,0 +1,12 @@
+package com.isoft.service;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IUserService
+{
+    String login(String uname, String upwd);
+    boolean register(String uname,String upwd,String email);
+    List<Map<String,Object>> findAllUser(int page, int pageSize);
+    Map<String,Object> findUserCount();
+}
